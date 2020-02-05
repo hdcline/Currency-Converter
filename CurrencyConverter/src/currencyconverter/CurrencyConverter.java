78bd95522f6dcbcbbabe3186077879277cdfb412
hdcline <huntercline12@gmail.com>
2020-02-05 13:37:50 -0500

    Testing changes to source file

--- CurrencyConverter/src/currencyconverter/CurrencyConverter.java
+++ CurrencyConverter/src/currencyconverter/CurrencyConverter.java
@@ -15,7 +15,7 @@ public class CurrencyConverter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //First comment. Testing git commit.
     }
     
 }
